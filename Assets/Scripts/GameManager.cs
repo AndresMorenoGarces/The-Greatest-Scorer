2d2ea7e2f782643bccc7b3a1d77fba1fd7d16ac5
Luxfero000 <andres999moreno@outlook.com>
2019-09-05 16:54:41 -0500

    Update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
@@ -15,6 +14,7 @@ public class GameManager : MonoBehaviour
 
     public Transform[] LeftSpikes;
     public Transform[] RightSpikes;
+    public Transform particlesTransform;
 
     int score = 0;
     int randomSpike = 0;
@@ -23,46 +23,50 @@ public class GameManager : MonoBehaviour
 
     [HideInInspector]
     public int colorInt = 0;
+    int particlesColorInt = -1;
     int ballNumber = 0;
 
     float adittionForScore = 0;
     float gameVelocity = 0.075f;
+    float currentTimeScale = 1;
+    float numberParticles = 0;
 
     public Button buttonBegin;
     public Button buttonRestart;
-    //public Button buttonPause;
+    public Button buttonPause;
+    public Button exitButton;
+    public Button instructionButton;
 
     public Text beginText;
     public Text restartText;
-
     public Text title;
     public Text credits;
     public Text counter;
-
+    public Text instructionText;
     public Text lastScoreText;
     public Text bestScoreText;
 
+    public Canvas pauseCanvas;
     public Image counterImage;
 
     public Color[] colorList;
+    public Gradient[] colorParticlesList;
 
     public Sprite[] ballSprites;
-
     public AudioClip ballSound;
-
-    public AudioClip wallSound;  
-
+    public AudioClip wallSound;
     public AudioClip loseSound;
-
     public AudioClip firstSong;
-
     public AudioClip secondSong;
 
     AudioSource audioSource;
-    
 
+    bool loadFirstSong = false;
     bool active;
 
+    public ParticleSystem particleSystemBall;
+    ParticleSystem.EmissionModule emissionModule;
+    ParticleSystem.ColorOverLifetimeModule colorOverLifetimeModule;
 
     public void FunctionsActivator()
     {
@@ -72,16 +76,18 @@ public class GameManager : MonoBehaviour
             if (gameVelocity <= 1f)
             {
                 Time.timeScale += gameVelocity;
+                currentTimeScale = Time.timeScale;
             }
             if (score % 10 == 0)
             {
                 ChanceBallSprite();
+                UpgradeParticlesBall();
                 if (adittionForScore < 6)
                 {
-                    adittionForScore++;    
+                    adittionForScore++;
                 }
             }
-        } 
+        }
     }
 
     public void BeginGame()
@@ -93,29 +99,93 @@ public class GameManager : MonoBehaviour
         credits.gameObject.SetActive(false);
         counter.gameObject.SetActive(true);
         counterImage.gameObject.SetActive(true);
-        //buttonPause.gameObject.SetActive(true);
+        buttonPause.gameObject.SetActive(true);
+        instructionButton.gameObject.SetActive(false);
+        buttonPause.gameObject.SetActive(true);
     }
 
     void LoseGame()
     {
         if (ball == null)
         {
-            counter.text = "You Lose";
+            if (score > 10 && score <= 20)
+            {
+                counter.text = " Good \n" + score;
+            }
+            else if (score > 20 && score <= 30)
+            {
+                counter.text = " Cool \n" + score;
+            }
+            else if (score > 30 && score <= 40)
+            {
+                counter.text = " Amazing \n" + score;
+            }
+            else if (score > 40 && score <= 50)
+            {
+                counter.text = " Boss \n" + score;
+            }
+            else if (score > 50 && score <= 60)
+            {
+                counter.text = " Titan \n" + score;
+            }
+            else if (score > 60 && score <= 70)
+            {
+                counter.text = " Unreal \n" + score;
+            }
+            else if (score > 70 && score <= 80)
+            {
+                counter.text = " Real.Pro \n" + score;
+            }
+            else if (score > 80 && score <= 90)
+            {
+                counter.text = " Wizard \n" + score;
+            }
+            else if (score > 90 && score <= 100)
+            {
+                counter.text = " The.Best \n" + score;
+            }
+            else if (score > 100 && score < 200)
+            {
+                counter.text = " Legend \n" + score;
+            }
+            else if (score >= 200)
+            {
+                counter.text = " God \n" + score;
+            }
+            else
+                counter.text = " Again \n" + score;
+
             restartText.text = "RESTART";
             buttonRestart.gameObject.SetActive(true);
+            buttonPause.gameObject.SetActive(false);
         }
     }
 
-    //public void PauseMode()
-    //{
-    //        active = !active;
-    //        Time.timeScale = (active) ? 0 : 1;
-    //}
+    public void PauseMode()
+    {
+        active = !active;
+
+        Time.timeScale = (active) ? 0 : currentTimeScale;
+        pauseCanvas.gameObject.SetActive(active);
+    }
 
     public void RestartGame()
     {
         SceneManager.LoadScene("SampleScene");
         beginText.text = "START";
+    }
+
+    public void Instructions()
+    {
+        instructionText.gameObject.SetActive(true);
+        instructionButton.gameObject.SetActive(false);
+        exitButton.gameObject.SetActive(true);
+        title.gameObject.SetActive(false);
+        buttonBegin.gameObject.SetActive(false);
+        lastScoreText.gameObject.SetActive(false);
+        bestScoreText.gameObject.SetActive(false);
+        credits.gameObject.SetActive(false);
+
     }
 
     public void UpdateScore()
@@ -126,7 +196,7 @@ public class GameManager : MonoBehaviour
     }
 
     public void SaveTemporalScore()
-    {      
+    {
         lastScore = score;
         lastScoreText.text = "Last Score:\n" + PlayerPrefs.GetInt("Last_Score");
         PlayerPrefs.SetInt("Last_Score", lastScore);
@@ -146,15 +216,15 @@ public class GameManager : MonoBehaviour
 
     void LoadScore()
     {
-       lastScoreText.text = "Last Score:\n" + PlayerPrefs.GetInt("Last_Score");
-       bestScoreText.text = "Best Score:\n" + PlayerPrefs.GetInt("Best_Score");
+        lastScoreText.text = "Last Score:\n" + PlayerPrefs.GetInt("Last_Score");
+        bestScoreText.text = "Best Score:\n" + PlayerPrefs.GetInt("Best_Score");
     }
 
     public void ChangeSpikePosition(bool leftWall)
     {
         if (leftWall == true)
         {
-            for (int i = 0; i < Random.Range(1 + adittionForScore, RightSpikes.Length - 6 + adittionForScore); i++)
+            for (int i = 0; i < Random.Range(0 + adittionForScore, RightSpikes.Length - 7 + adittionForScore); i++)
             {
                 for (int j = 0; j < LeftSpikes.Length; j++)
                 {
@@ -167,7 +237,7 @@ public class GameManager : MonoBehaviour
         }
         else if (leftWall == false)
         {
-            for (int i = 0; i < Random.Range(1 + adittionForScore, LeftSpikes.Length -6 + adittionForScore); i++)
+            for (int i = 0; i < Random.Range(1 + adittionForScore, LeftSpikes.Length - 7 + adittionForScore); i++)
             {
                 for (int j = 0; j < RightSpikes.Length; j++)
                 {
@@ -177,7 +247,7 @@ public class GameManager : MonoBehaviour
                 LeftSpikes[randomSpike].gameObject.SetActive(true);
                 SpikesColor(LeftSpikes[randomSpike].gameObject);
             }
-        }    
+        }
     }
 
     void SpikesColor(GameObject _spike)
@@ -195,9 +265,26 @@ public class GameManager : MonoBehaviour
             ballNumber++;
             ball.GetComponent<SpriteRenderer>().sprite = ballSprites[ballNumber];
         }
-        else 
+        else
         {
             ballNumber = 0;
+        }
+    }
+
+    void UpgradeParticlesBall()
+    {
+        particlesColorInt++;
+
+        if (numberParticles <= 100)
+        {
+            numberParticles += 10;
+        }
+        emissionModule.rateOverTime = numberParticles;
+        colorOverLifetimeModule.color = colorParticlesList[particlesColorInt];
+
+        if (particlesColorInt == colorParticlesList.Length)
+        {
+            particlesColorInt = 0;
         }
     }
 
@@ -229,7 +316,8 @@ public class GameManager : MonoBehaviour
     {
         if (audioSource.isPlaying == false)
         {
-            audioSource.clip = firstSong;
+            loadFirstSong = !loadFirstSong;
+            audioSource.clip = loadFirstSong ? firstSong  :  secondSong;
             audioSource.Play();
         }
     }
@@ -240,8 +328,8 @@ public class GameManager : MonoBehaviour
         credits.text = "Developer \nAndrés F. Moreno Garcés";
         audioSource = musicContainer.GetComponent<AudioSource>();
         ball.SetActive(false);
-        //buttonPause.gameObject.SetActive(false);
-
+        emissionModule = particleSystemBall.emission;
+        colorOverLifetimeModule = particleSystemBall.colorOverLifetime;       
 
         if (instance == null)
         {
